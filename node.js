f6c8b27ae9c3c30e8f5fb095dccb8ff18f5f5a0e
Renann Souza <143777162+Renann2309@users.noreply.github.com>
2023-09-13 23:34:46 -0300

    Add files via upload

---
+++ node.js
@@ -0,0 +1,20 @@
+//sem parametro
+function soma()
+{
+    
+}
+
+//retorno de valor:
+
+function soma (a, b){
+    return a + b;
+}
+var resultado = soma (1, 8);
+console.log("o resultado foi: " + resultado);
+
+//arrow com valor
+(c, d) => {
+    return c - d;
+}
+var resultado2 = (8, 4);
+console.log(resultado2);
