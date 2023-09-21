1361a99ccd811f4b5ae7ca5c6d9a726622c75c08
Renann Souza <143777162+Renann2309@users.noreply.github.com>
2023-09-21 00:05:41 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+        username: 'Renann Souza',
+        password: '123456',
+        database: 'teste',
+        host: 'localhost',
+        dialect: 'mysql'
+
+    }
+};
